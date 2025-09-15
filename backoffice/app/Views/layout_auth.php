6f3f357e6ffe778e1cd220fc1e4e3284eeca8016
sidemarschimmelpfennig <sidemarschimmelpfennig@outlook.com>
2025-09-14 23:22:03 -0300

    layout login create

---
+++ backoffice/app/Views/layout_auth.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CigBurger Backoffice</title>
+
+    <!-- bootstrap -->
+    <link rel="stylesheet" href="<?= base_url("assets/library/bootstrap/bootstrap.min.css") ?>">
+
+    <!-- fontawesome -->
+    <link rel="stylesheet" href="<?= base_url("assets/library/fontawesome/all.min.css") ?>">
+
+    <!-- style css -->
+    <link rel="stylesheet" href="<?= base_url("assets/css/login.css") ?>">
+</head>
+
+<body>
+    <!-- render section -->
+    <?= $this->renderSection("content") ?>
+
+    <!-- import script bootstrap  -->
+    <script src="<?= base_url("assets/library/bootstrap/bootstrap.bundle.min.js") ?>"></script>
+</body>
+
+</html>
